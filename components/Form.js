635a45000a1b1bfd65eacc751398a60e187c6736
Abraham Hurtado <arhc.hc@gmail.com>
2018-05-11 00:15:59 -0700

    Form hack

--- components/Form.js
+++ components/Form.js
@@ -1,27 +1,35 @@
 import { Form, Button, FormGroup, Label, Input, FormText } from "reactstrap";
 
-export default () => (
-  <Form>
-    <FormGroup>
-      <Label for="nombreInput">Nombre</Label>
-      <Input type="text" name="nombre" id="nombreInput" />
-    </FormGroup>
-    <FormGroup>
-      <Label for="emailInput">Correo</Label>
-      <Input type="email" name="email" id="emailInput" />
-    </FormGroup>
-    <FormGroup>
-      <Label for="ciudadInput">Ciudad</Label>
-      <Input type="text" name="ciudad" id="ciudadInput" />
-    </FormGroup>
-    <FormGroup>
-      <Label for="telefonoInput">Teléfono</Label>
-      <Input type="tel" name="telefono" id="telefonoInput" />
-    </FormGroup>
-    <FormGroup>
-      <Label for="mensajeInput">Mensaje</Label>
-      <Input type="textarea" name="mensaje" label="mensajeInput" />
-    </FormGroup>
-    <Button>Enviar</Button>
-  </Form>
-);
+export default class FormCustom extends React.Component {
+  submit = e => {
+    e.preventDefault();
+    setTimeout(() => alert("Se envió exitosamente tu mensaje"), 1500);
+  };
+  render() {
+    return (
+      <Form onSubmit={this.submit}>
+        <FormGroup>
+          <Label for="nombreInput">Nombre</Label>
+          <Input type="text" name="nombre" id="nombreInput" required />
+        </FormGroup>
+        <FormGroup>
+          <Label for="emailInput">Correo</Label>
+          <Input type="email" name="email" id="emailInput" required />
+        </FormGroup>
+        <FormGroup>
+          <Label for="ciudadInput">Ciudad</Label>
+          <Input type="text" name="ciudad" id="ciudadInput" required />
+        </FormGroup>
+        <FormGroup>
+          <Label for="telefonoInput">Teléfono</Label>
+          <Input type="tel" name="telefono" id="telefonoInput" required />
+        </FormGroup>
+        <FormGroup>
+          <Label for="mensajeInput">Mensaje</Label>
+          <Input type="textarea" name="mensaje" label="mensajeInput" required />
+        </FormGroup>
+        <Button>Enviar</Button>
+      </Form>
+    );
+  }
+}
